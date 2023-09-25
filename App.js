9dcb0cf4bc0b11c3f0a589096cde38044422d25a
hyunwoomemo <archihw94@gmail.com>
2023-09-25 23:20:49 +0900

    init: firebase 셋팅

--- App.js
+++ App.js
@@ -1,8 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import auth from '@react-native-firebase/auth'
 
 export default function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+  useEffect(() => {
+    auth().onAuthStateChanged((user) => {
+      if (user) {
+        setIsLoggedIn(true)
+      } else {
+        setIsLoggedIn(false)
+      }
+    })
+  })
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
